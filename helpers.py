53300f92970e1c48762441ce5eeb6e32984d760c
AstradaT <tomi_cuervocasla@hotmail.com>
2023-01-31 03:06:19 -0300

    Move avro schemas

--- helpers.py
+++ helpers.py
@@ -6,3 +6,37 @@ def get_data(table: str):
     cur = conn.cursor()
     result = cur.execute(f"SELECT * FROM {table};").fetchall()
     return result
+
+
+schema_d = {
+    'name': 'Departments',
+    'namespace': 'globant',
+    'type': 'record',
+    'fields': [
+        {'name': 'id', 'type': 'int'},
+        {'name': 'department', 'type': 'string'},
+    ],
+    }
+
+schema_e = {
+    'name': 'Employees',
+    'namespace': 'globant',
+    'type': 'record',
+    'fields': [
+        {'name': 'id', 'type': 'int'},
+        {'name': 'name', 'type': 'string'},
+        {'name': 'datetime', 'type': 'string'},
+        {'name': 'department_id', 'type': 'int'},
+        {'name': 'job_id', 'type': 'int'},
+    ],
+    }
+
+schema_j = {
+    'name': 'Jobs',
+    'namespace': 'globant',
+    'type': 'record',
+    'fields': [
+        {'name': 'id', 'type': 'int'},
+        {'name': 'job', 'type': 'string'},
+    ],
+    }
