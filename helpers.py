caa12235375c8eb71590e44229b31d51d55a9265
AstradaT <tomi_cuervocasla@hotmail.com>
2023-01-31 16:59:04 -0300

    Make function to read avro files

--- helpers.py
+++ helpers.py
@@ -1,11 +1,21 @@
 import sqlite3
+from fastavro import reader
+import json
 
 
-def get_data(table: str):
+def get_data(table:str, offset:int, limit:int):
     conn = sqlite3.connect('instance/database.db')
     cur = conn.cursor()
-    result = cur.execute(f"SELECT * FROM {table};").fetchall()
+    result = cur.execute(f"SELECT * FROM {table} LIMIT {limit} OFFSET {offset};").fetchall()
     return result
+
+
+def read_avro(table: str):
+    with open(f'data/{table}.avro', 'rb') as file:
+        avro_reader = reader(file)
+        records = [r for r in avro_reader]
+
+    return json.dumps(records)
 
 
 schema_d = {
@@ -24,10 +34,10 @@ schema_e = {
     'type': 'record',
     'fields': [
         {'name': 'id', 'type': 'int'},
-        {'name': 'name', 'type': 'string'},
-        {'name': 'datetime', 'type': 'string'},
-        {'name': 'department_id', 'type': 'int'},
-        {'name': 'job_id', 'type': 'int'},
+        {'name': 'name', 'type': ['string', 'null']},
+        {'name': 'datetime', 'type': ['string', 'null']},
+        {'name': 'department_id', 'type': ['int', 'null']},
+        {'name': 'job_id', 'type': ['int', 'null']},
     ],
     }
 
