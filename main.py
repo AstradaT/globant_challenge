77768ea3a6cb6ed3101005361f44f697d9ea2294
AstradaT <tomi_cuervocasla@hotmail.com>
2023-01-30 22:03:24 -0300

    Add API routes

--- main.py
+++ main.py
@@ -2,18 +2,16 @@ from flask import Flask, jsonify, request, render_template, flash
 from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
 import os
+import sqlite3
+from sqlalchemy.exc import IntegrityError
 
 
 app = Flask(__name__)
+app.secret_key = 'hello'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['UPLOAD_FOLDER'] = 'data'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-
-
-# departments = pd.read_csv('data/raw/departments.csv', names=['id','department'], header=None).to_json()
-# hired_employees = pd.read_csv('data/raw/hired_employees.csv', names=['id','name','datetime','department_id','job_id'], header=None).to_json()
-# jobs = pd.read_csv('data/raw/jobs.csv', names=['id','job'], header=None).to_json()
 
 
 class Department(db.Model):
@@ -46,10 +44,6 @@ class Job(db.Model):
         return f"<Job(id={self.id}, job={self.job})>"
 
 
-with app.app_context():
-    db.create_all()
-
-
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
@@ -62,31 +56,74 @@ def home():
             # Convert csv to dataframe
             df = pd.read_csv(filepath, header=None)
             # Move data to SQLite database
-            with db.engine.begin() as connection:
+            with db.engine.begin() as conn:
                 table = file.filename
-                df.to_sql(table, con=connection, if_exists='replace', index=False, chunksize=1000)
+                df.to_sql(table, con=conn, if_exists='replace', index=False, chunksize=1000)
                 table_2 = table.removeprefix('hired_').removesuffix('.csv')
-                connection.execute(f"INSERT INTO {table_2} SELECT * FROM `{table}`;")
-        success_message = "The CSV file has been uploaded to the database successfully."
-        flash(success_message)
-            
+                try:
+                    conn.execute(f"INSERT INTO {table_2} SELECT * FROM `{table}`;")
+                    message = "The CSV file has been uploaded to the database successfully."
+                except IntegrityError:
+                    message = "This data is already in the database."
+        flash(message)
     return render_template('index.html')
 
 
-@app.route('/departments', methods=['GET'])
+@app.route('/api/departments', methods=['GET'])
 def get_departments():
-    return jsonify(departments)
+    conn = sqlite3.connect('instance/database.db')
+    cur = conn.cursor()
+    result = cur.execute("SELECT * FROM departments;").fetchall()
+    lis = []
+    for row in result:
+        lis.append({'id': row[0], 'department': row[1]})
+    return f"{lis}"
 
 
-@app.route('/employees', methods=['GET'])
+@app.route('/api/employees', methods=['GET'])
 def get_hired_employees():
-    return jsonify(hired_employees)
+    conn = sqlite3.connect('instance/database.db')
+    cur = conn.cursor()
+    result = cur.execute("SELECT * FROM employees;").fetchall()
+    lis = []
+    for row in result:
+        lis.append({
+            'id': row[0], 
+            'name': row[1], 
+            'datetime': row[2], 
+            'department_id': row[3], 
+            'job_id': row[4]})
+    return f"{lis}"
 
 
-@app.route('/jobs', methods=['GET'])
+@app.route('/api/jobs', methods=['GET'])
 def get_jobs():
-    return jsonify(jobs)
+    conn = sqlite3.connect('instance/database.db')
+    cur = conn.cursor()
+    result = cur.execute("SELECT * FROM jobs;").fetchall()
+    lis = []
+    for row in result:
+        lis.append({'id': row[0], 'job': row[1]})
+    return f"{lis}"
+
+
+@app.route('/api/departments/backup', methods=['GET'])
+def backup_departments():
+    pass
+
+
+@app.route('/api/employees/backup', methods=['GET'])
+def backup_hired_employees():
+    pass
+
+
+@app.route('/api/jobs/backup', methods=['GET'])
+def backup_jobs():
+    pass
 
 
 if __name__ == '__main__':
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
     app.run(debug=True, host="0.0.0.0", port=5000)
