ef5af200e5d9152d3c54ebd8da6c4eab0b284037
AstradaT <tomi_cuervocasla@hotmail.com>
2023-02-02 11:34:45 -0300

    Set debug off

--- main.py
+++ main.py
@@ -229,4 +229,4 @@ if __name__ == '__main__':
     db.init_app(app)
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=False, host="0.0.0.0", port=5000)
